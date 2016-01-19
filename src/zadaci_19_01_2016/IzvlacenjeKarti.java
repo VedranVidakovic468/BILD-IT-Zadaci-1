6e46439676b0464f1fcb3b278a628f31b404865b
krafthouse <ahmulja@gmail.com>
2016-01-20 00:28:46 +0100

    19_01_2016
    dodavanje kometara za program izvlacenje karti

--- src/zadaci_19_01_2016/IzvlacenjeKarti.java
+++ src/zadaci_19_01_2016/IzvlacenjeKarti.java
@@ -6,13 +6,14 @@ public class IzvlacenjeKarti {
 
 	public static void main(String[] args) {
 		
-		Random randomGenerator = new Random();
+		Random randomGenerator = new Random(); // random objekat
 		
+		// niz string sa imenima karata
 		String [] karte = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q" , "K"};
-		String [] znakovi = {"Srce", "Pik", "Djetelina", "Kocka"};
+		String [] znakovi = {"Srce", "Pik", "Djetelina", "Kocka"}; // niz string sa imenima znakova karata
 
-		int brojKarte = randomGenerator.nextInt(13);
-		int brojZnaka = randomGenerator.nextInt(4);
+		int brojKarte = randomGenerator.nextInt(13); //generise nasumican broj izmedju 0-13 
+		int brojZnaka = randomGenerator.nextInt(4); //generise nasumican brojs izmedju 0-4
 		
 		System.out.println("Karta koju ste izvukli je "+karte[brojKarte]+" u znaku "+znakovi[brojZnaka]);
 	}
